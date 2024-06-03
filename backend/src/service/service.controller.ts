a62f539fd0dc660e9a2faafb9ad1bb018664ac69
nkshn <vovanika23413@gmail.com>
2024-06-03 19:40:35 +0300

    added seeds for service and update service module

--- backend/src/service/service.controller.ts
+++ backend/src/service/service.controller.ts
@@ -1,52 +1,15 @@
 import {
 	Controller,
 	Get,
-	Post,
-	Body,
-	Param,
-	Delete,
-	HttpCode,
-	UsePipes,
-	ValidationPipe,
-	Put
 } from "@nestjs/common"
 import { ServiceService } from "./service.service"
-import { CreateServiceDto, UpdateServiceDto } from "./dto/service.dto"
 
 @Controller("service")
 export class ServiceController {
-	constructor(private readonly serviceService: ServiceService) {}
+	constructor(private readonly serviceService: ServiceService) { }
 
 	@Get()
 	async getAll() {
 		return this.serviceService.getAll()
-	}
-
-	@Get(":id")
-	async getOne(@Param("id") id: string) {
-		return this.serviceService.getOneById(id)
-	}
-
-	// TODO: add admin guard
-	@UsePipes(new ValidationPipe())
-	@HttpCode(200)
-	@Post()
-	async create(@Body() dto: CreateServiceDto) {
-		return this.serviceService.create(dto)
-	}
-
-	// TODO: add admin guard
-	@UsePipes(new ValidationPipe())
-	@HttpCode(200)
-	@Put(":id")
-	async update(@Param("id") serviceId: string, @Body() dto: UpdateServiceDto) {
-		return this.serviceService.update(serviceId, dto)
-	}
-
-	// TODO: add admin guard
-	@HttpCode(200)
-	@Delete(":id")
-	async delete(@Param("id") serviceId: string) {
-		return this.serviceService.delete(serviceId)
 	}
 }
--- backend/src/service/service.service.ts
+++ backend/src/service/service.service.ts
@@ -1,37 +1,20 @@
 import { Injectable } from "@nestjs/common"
 import { PrismaService } from "src/prisma.service"
-import { CreateServiceDto, UpdateServiceDto } from "./dto/service.dto"
 
 @Injectable()
 export class ServiceService {
 	constructor(private prisma: PrismaService) {}
 
 	async getAll() {
-		return this.prisma.service.findMany()
-	}
-
-	async getOneById(serviceId: string) {
-		return this.prisma.service.findFirst({
-			where: { id: serviceId }
+		const services = await this.prisma.service.findMany({
+			select: {
+				id: true,
+				fullName: true,
+				shortName: true,
+				backgroundColor: true,
+			}
 		})
-	}
 
-	async create(dto: CreateServiceDto) {
-		return this.prisma.service.create({
-			data: dto
-		})
-	}
-
-	async update(serviceId: string, dto: UpdateServiceDto) {
-		return this.prisma.service.update({
-			where: { id: serviceId },
-			data: dto
-		})
-	}
-
-	async delete(serviceId: string) {
-		return this.prisma.service.delete({
-			where: { id: serviceId }
-		})
+		return { services }
 	}
 }
