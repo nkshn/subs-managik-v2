bee3780c9aabfa224f9e810cf3d17d123c159e52
nkshn <vovanika23413@gmail.com>
2024-06-13 19:32:58 +0300

    fixes before production v5

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -5,29 +5,33 @@ import { NextRequest, NextResponse } from 'next/server'
 export async function middleware(request: NextRequest, response: NextResponse) {
   const { url, cookies } = request
   const refreshToken = cookies.get(EnumTokens.REFRESH_TOKEN)?.value
-  const isLoginPage = url.includes(APP_PAGES.LOGIN)
-  const isRegisterPage = url.includes(APP_PAGES.REGISTER)
-  const isAuthPage = isLoginPage || isRegisterPage
+  const isAuthPage = url.includes(APP_PAGES.LOGIN) || url.includes(APP_PAGES.REGISTER)
   const isProfilePage = url.includes(APP_PAGES.PROFILE)
 
+  // If user is on an auth page and has a refresh token, redirect to subscriptions
   if (isAuthPage && refreshToken) {
     return NextResponse.redirect(new URL(APP_PAGES.SUBSCRIPTIONS, url))
   }
 
+  // If user is trying to access the profile page without a refresh token, redirect to login
+  if (isProfilePage && !refreshToken) {
+    return NextResponse.redirect(new URL(APP_PAGES.LOGIN, url))
+  }
+
+  // If user is on an auth page and does not have a refresh token, allow access
   if (isAuthPage) {
     return NextResponse.next()
   }
 
+  // If user does not have a refresh token, redirect to login
   if (!refreshToken) {
-    if (isProfilePage) {
-      return NextResponse.redirect(new URL(APP_PAGES.LOGIN, request.url))
-    }
-    return NextResponse.redirect(new URL(APP_PAGES.LOGIN, request.url))
+    return NextResponse.redirect(new URL(APP_PAGES.LOGIN, url))
   }
 
+  // Allow access to all other pages
   return NextResponse.next()
 }
 
 export const config = {
-  matcher: ['/login', '/register', '/profile', '/:path'],
+  matcher: ['/profile', '/login', '/register'],
 }
